3227cfcb0d2bee6d41bba098f84b64312c6c2193
GarrenSmith <garren.smith@gmail.com>
2011-02-11 11:45:55 +0200

    removed personal api key from demo

--- examples/demo.js
+++ examples/demo.js
@@ -1,14 +1,13 @@
 var Madmimi = require('../lib/index');
 
-var madmimi = new Madmimi("garren.smith@gmail.com","d152284b309e8bb771524e88d7772b9e");
+var madmimi = new Madmimi("email","api_key");
 
 var options = {
-      promotion_name:"Test Promotion",
-      recipient:"Garren Smith <garren.smith@gmail.com>",
+       promotion_name:"Test Promotion",
+      recipient:"Jimi Hendrix <jimi@electricladyland.com>",
       subject:"Test Promotion",
-    //bcc:"admin@example.com",
-    from:"no-reply@classroom7.com",
-    raw_html:"<html><head><title>My great promotion!</title></head><body>Body stuff[[tracking_beacon]]</body></html>"
+    from:"no-reply@guitargear.com",
+    raw_html:"<html><head><title>Great promotion!</title></head><body>Cool guitar stuff[[tracking_beacon]]</body></html>"
 };
 
 madmimi.sendMail(options, function (msg) {
