e49250f3b5897396b5297238ee5667fe9f536178
GarrenSmith <garren.smith@gmail.com>
2011-02-12 15:08:56 +0200

    rough promotion list with failing tests

--- examples/demo.js
+++ examples/demo.js
@@ -1,19 +1,24 @@
 var Madmimi = require('../lib/index');
+    
+var madmimi = new Madmimi(process.env['email'],process.env["madmimi_api_key"], true);
 
-var madmimi = new Madmimi("email","api_key");
+/*var options = {
+  promotion_name:"Test Promotion",
+  recipient:"Jimi Hendrix <jimi@electricladyland.com>",
+  subject:"Test Promotion",
+  from:"no-reply@guitargear.com",
+  raw_html:"<html><head><title>Great promotion!</title></head><body>Cool guitar stuff[[tracking_beacon]]</body></html>"
+  };
 
-var options = {
-       promotion_name:"Test Promotion",
-      recipient:"Jimi Hendrix <jimi@electricladyland.com>",
-      subject:"Test Promotion",
-    from:"no-reply@guitargear.com",
-    raw_html:"<html><head><title>Great promotion!</title></head><body>Cool guitar stuff[[tracking_beacon]]</body></html>"
-};
-
-madmimi.sendMail(options, function (msg) {
+  madmimi.sendMail(options, function (msg) {
   console.log("email send, the id is: " + msg);
-});
+  });
 
-madmimi.mailStatus(2861240550, function (msg) {
+  madmimi.mailStatus(2861240550, function (msg) {
   console.log("status: " + msg);
+  });*/
+
+
+madmimi.promotions(function (promotions) {  
+  console.dir(promotions);  
 });
---
+++ spec/promotions_spec.js
@@ -0,0 +1,34 @@
+var describe = require('Jody').describe,
+    Madmimi = require('../lib/index.js');
+
+var madmimi = "";
+var email_options = {
+      promotion_name:"Test Promotion",
+      recipient:"Jimi Hendrix <jimi@electricladyland.com>",
+      subject:"Test Promotion",
+    from:"no-reply@guitargear.com",
+    raw_html:"<html><head><title>Great promotion!</title></head><body>Cool guitar stuff[[tracking_beacon]]</body></html>"
+};
+
+
+describe("Promotions").
+  beforeEach( function () {
+    madmimi = new Madmimi("fake@email.com", "fake-api-key");
+  }).  
+  it("Should create http request for all promotions", function (atEnd) {
+      var requestOptions; 
+    
+    madmimi.request = function (options, body) {
+     requestOptions = options;
+    };
+
+    madmimi.promotions(function () {});
+
+    atEnd(function () {
+      requestOptions.host.should().beEqual('madmimi.com');
+      requestOptions.port.should().beEqual('443');
+      requestOptions.path.should().beEqual('/mailers/status/1234?username=fake%40email.com&api_key=fake-api-key');
+      requestOptions.method.should().beEqual('GET');
+    })
+  });
+
