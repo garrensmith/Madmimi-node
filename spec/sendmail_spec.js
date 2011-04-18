8cfcc0e12a68efcba0568e126320c4e44c7baf0c
GarrenSmith <garren.smith@gmail.com>
2011-04-18 14:28:50 +0200

    sending jade email

--- spec/sendmail_spec.js
+++ spec/sendmail_spec.js
@@ -44,14 +44,18 @@ describe("Render Jade").
   }).
   it("Should render to html", function (async) {
     madmimi.request = async(function (requestOptions, body) {
-     body.should().beEqual('promotion_name=Test%20Promotion&recipient=Jimi%20Hendrix%20%3Cjimi%40electricladyland.com%3E&subject=Test%20Promotion&from=no-reply%40guitargear.com&raw_html=%3Chtml%3E%3Chead%3E%3Ctitle%3EGreat%20promotion!%3C%2Ftitle%3E%3C%2Fhead%3E%3Cbody%3ECool%20guitar%20stuff%5B%5Btracking_beacon%5D%5D%3C%2Fbody%3E%3C%2Fhtml%3E&username=fake%40email.com&api_key=fake-api-key'); 
-
+     body.should().beEqual("promotion_name=Test%20Promotion&recipient=Jimi%20Hendrix%20%3Cjimi%40electricladyland.com%3E&subject=Test%20Promotion&from=no-reply%40guitargear.com&username=fake%40email.com&api_key=fake-api-key&raw_html=%3C!DOCTYPE%20html%3E%3Chead%3E%3Ctitle%3EGreat%20promotion!%3C%2Ftitle%3E%3C%2Fhead%3E%3Cbody%3E%3Cp%3ECool%20guitar%20stuff%20%5B%5Btracking_beacon%5D%5D%0A%3C%2Fp%3E%3C%2Fbody%3E");
 
 
     });
 
-    var options = email_options;
-    delete options.raw_html;
+    var options = {
+      promotion_name:"Test Promotion",
+      recipient:"Jimi Hendrix <jimi@electricladyland.com>",
+      subject:"Test Promotion",
+      from:"no-reply@guitargear.com",
+    };
+
     options.jade = process.cwd() + '/spec/fixtures/email.jade';
 
 
